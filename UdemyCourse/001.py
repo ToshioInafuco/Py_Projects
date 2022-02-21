2eeb87c8e833f094d6374ff04f48f4d8eb604319
ToshioInafuco <toshioin07@gmail.com>
2022-02-21 08:35:46 -0300

    Commit realizado dia 21/02

---
+++ UdemyCourse/001.py
@@ -0,0 +1,33 @@
+"""   AULA 2   !!!
+104.481.679-13
+"""
+print("104", "481", "679", sep=".", end="-")
+print("13")
+
+
+"""   AULA 4   !!!
+Tipos de dados:       type() #confere a classe
+str - string - textos -> "Ex" ou 'ex'
+int - inteiro - números inteiros(+ ou -)
+float - números com casas decimais
+bool - booleano/lógico - True/False
+"""
+
+"""   AULA 5   !!!
+(), **, *, /, //, %, +, -    !!!Já em ordem de prioridade!!!
+"""
+print(10 * 10)
+print(10 + 10)
+print(10 - 5)
+print(10 ** 2)#potência
+print(100 // 2)#divisão inteira!!
+print(10 % 2)#resto da divisão
+
+print("#"*42)#para repetir a string x vezes
+
+
+"""   AULA 6   !!!
+Iniciar com letra, pode conter números, separar por "_"
+"""
+
+
